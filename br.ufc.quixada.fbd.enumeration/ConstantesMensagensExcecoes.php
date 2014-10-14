0608dd58cd05ebf144c62e6cacb7168cb9d4f070
spectrus <spectrus@spectrus-POSITIVO-MOBILE>
2014-10-14 19:58:36 -0300

    Criação de enumeration para mensagens de exceções Mudança de pacotes e caminhos

---
+++ br.ufc.quixada.fbd.enumeration/ConstantesMensagensExcecoes.php
@@ -0,0 +1,8 @@
+<?php
+	abstract class ConstantesMensagensExcecoes{
+		const 
+		FALHA_AO_EXECUTAR_QUERY = 'Ocorreu alguma falha na execu&ccedil;&atilde;o da query',
+		PREPARE_STATEMENT_FALHA = 'Erro ao criar prepare statement',
+		FALHA_AO_CRIAR_CONEXAO = 'N&atilde;o foi poss&iacute;vel criar a conex&atilde;o';
+	}
+?>
